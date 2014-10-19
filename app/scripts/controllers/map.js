636437f42f97b62310ae14483329cbc53763d8b3
kawamoto <kawamoto@nihon-protec.co.jp>
2014-10-20 02:32:57 +0900

    commit

--- app/scripts/controllers/map.js
+++ app/scripts/controllers/map.js
@@ -16,4 +16,36 @@ angular.module('appApp')
             },
             zoom: 14
         };
+        $scope.options = {scrollwheel: false};
+        $scope.coordsUpdates = 0;
+        $scope.dynamicMoveCtr = 0;
+        $scope.marker = {
+            id: 0,
+            coords: {
+                latitude: 35.681382,
+                longitude: 139.766084
+            },
+            options: { draggable: true },
+            events: {
+                dragend: function (marker, eventName, args) {
+                    $log.log('marker dragend');
+                    var lat = marker.getPosition().lat();
+                    var lon = marker.getPosition().lng();
+                    $log.log(lat);
+                    $log.log(lon);
+
+                    $scope.marker.options = {
+                        draggable: true,
+                        labelContent: "lat: " + $scope.marker.coords.latitude + ' ' + 'lon: ' + $scope.marker.coords.longitude,
+                        labelAnchor: "100 0",
+                        labelClass: "marker-labels"
+                    };
+                }
+            }
+        };
+        $scope.$watchCollection("marker.coords", function (newVal, oldVal) {
+            if (_.isEqual(newVal, oldVal))
+                return;
+            $scope.coordsUpdates++;
+        });
     });
