9854c63facebe2fa5829339b76a2057f875ac8da
kawamoto <kawamoto@nihon-protec.co.jp>
2014-10-25 17:05:41 +0900

    commit

--- app/scripts/controllers/charts.js
+++ app/scripts/controllers/charts.js
@@ -46,22 +46,23 @@ angular.module('appApp')
             console.log("createChart");
             var chart = {};
             chart.type = "LineChart";
-            chart.data = JSON.stringify($scope.chartData);
+            chart.data = $scope.chartData;
             chart.options = {
-               // title: '心拍数推移',
+                title: '【心拍数推移】',
                 dataOpacity: 0.5,
-                height: 500,
+                height: 400,
                 isStacked: true,
+                animation: { duration: 3000, easing: 'linear'},
                 hAxis: {
                     slantedText: true,
                     viewWindowMode: 'pretty',
                     textPosition:'out',
-                    title:'日付'
+                    title:'測定日'
                 },
                 vAxis: {
                     textPosition:'out',
                     title:'心拍数',
-                    ticks:[90,85,80,75,70,65,60,55,50]
+                    ticks:[110,105,100,95,90,85,80,75,70,65,60,55,50]
                 },
                 seriesType: "line"
 
